68bf8597e71984020bd0d39998d0d790ae05887a
mnalamaru4 <117784656+mnalamaru4@users.noreply.github.com>
2024-05-06 12:31:53 +1000

    updated

--- server1.js
+++ server1.js
@@ -82,8 +82,3 @@ app.post('/api/projects',(req,res) => {
 
 var port = process.env.port || 3000;
 
-app.listen(port,()=>{
-
- console.log("App listening to: http://localhost:"+port)
- createCollection("PETS")
-}
